e655387f3118da782ebd00a0548fa5b3ee2797a6
JohanEsquivel21 <55373557+JohanEsquivel21@users.noreply.github.com>
2019-09-26 10:37:40 -0500

    Create Hola mundo.py

---
+++ Hola mundo.py
@@ -0,0 +1,9 @@
+# Hola Mundo.py
+# Autor: Johan Alessandro Perez Esquivel
+# Fecha de creacion: 15/09/2019
+# Los comentarios solamente se utilizan para poder explicar un comando este no se ve cuando corres el programa.
+# El comando de print es para imprimir, por asi decirlo lo que se quiere correr al final, o para generar un escr
+ito.
+# Asi que, cuando usas el print siempre hay que poner el texto entre parentesis para que se pueda ejecutar corre
+ctamente.
+print("Hola Mundo, ahora en Python!")
